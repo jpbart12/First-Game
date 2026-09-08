e2e1de49a3313660b40440c9ac32b3e5ae90df2b
JP <jpbartlett05@gmail.com>
2026-09-07 19:13:45 -0600

    First Godot project

---
+++ gun.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+func _physics_process(delta):
+	var enimies_in_range = get_overlapping_bodies()
+	if enimies_in_range.size() > 0:
+			var target = enimies_in_range.front()
+			look_at(target.global_position)
+
+func shoot():
+	const BULLET = preload("res://bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout() -> void:
+	shoot()
